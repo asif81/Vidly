d4b7a57cda2c988f96fe06b74f68ae5e6fbdd71b
asif iqbal <asif_dcsku@hotmail.com>
2019-04-04 22:06:35 +0300

    .

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dto;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/Movies
+        private IHttpActionResult GetMovies()
+        {
+            var movieDto = _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+            return Ok(movieDto);
+        }
+
+        // GET /api/movies/1
+        private IHttpActionResult GetMovie(int Id)
+        {
+            Movie movie = _context.Movies.SingleOrDefault(c => c.MovieId == Id);
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        //POST /api/Movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDto.MovieId = movie.MovieId;
+            return Created(new Uri(Request.RequestUri + "/" + movie.MovieId), movieDto);
+        }
+
+        // PUT /api/Movies/1
+        public IHttpActionResult UpdateMovie(int Id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieIndDb = _context.Movies.SingleOrDefault(c => c.MovieId == Id);
+            if (movieIndDb == null)
+                return NotFound();
+
+            Mapper.Map(movieDto, movieIndDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE /api/Movies/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int Id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieIndDb = _context.Movies.SingleOrDefault(c => c.MovieId == Id);
+            if (movieIndDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieIndDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Vidly/Dto/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dto
+{
+    public class MovieDto
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MovieId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public int NoOfCopiesAvailable { get; set; }
+    }
+}
