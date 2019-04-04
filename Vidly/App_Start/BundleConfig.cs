c8c136ac5e8a1562d553e9be97d049ba8cae581e
asif iqbal <asif_dcsku@hotmail.com>
2019-04-04 23:25:44 +0300

    .

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace Vidly
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/sbootstrap.css",
                       "~/Content/site.css"));
+
+
         }
     }
 }
